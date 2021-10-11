f176de2c6666c1b95cf30b0cde1b10ec0a7b1bc9
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-11 23:12:34 +0100

    strange index

---
+++ strange.py
@@ -0,0 +1,6 @@
+print("What strange markings do you see?")
+markings = input()
+print("\nIdentifying...\n")
+
+for count in range(0, len(markings), 1):
+    print(f"index {count}:", markings[count])
