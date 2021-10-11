c16759d610d8ca0c41bb121a809e1b0eacf04bc0
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-11 23:25:52 +0100

    membership

---
+++ membership_ope.py
@@ -0,0 +1,13 @@
+print("What phrase do you see?")
+phrase = input()
+
+# Identify markings
+print("\nReversing...")
+print("The phrase is ", end="")
+
+reversed = ""
+
+for letter in phrase:
+    reversed = letter + reversed
+
+print(reversed)
