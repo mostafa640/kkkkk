58664a85b338e03386f2bee3094daf31db0dd590
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-11 22:12:17 +0100

    mountain

---
+++ mountain.py
@@ -0,0 +1,13 @@
+print("How many mountains should I display?")
+mountains = int(input())
+print("displaying...")
+for mountain in range(mountains):
+    print("""
+              __
+             /  \\_  
+            /^    \\
+           /  ^    \\_
+         _/ ^ ^     ^\\
+        /  ^     ^    \\
+
+     """)
